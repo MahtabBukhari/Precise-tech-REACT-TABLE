16a821104e10d44be975ab7579fa4b0ed8c95597
MahtabBukhari <git config --global user.email you@example.com>
2022-10-13 15:43:11 +0500

    initial Project start commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import {Routes, Route} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+app
+<Routes>
+     <Route  exact  path='/'/>
+     
+
+   
+
+    
+       
+  </Routes>
+  
+  
+ 
+    
+    </div>
+  )
+}
+
+export default App
